1ae2aeebe65dab8e4b6b2afe91e4578244b96286
Kenten Roth <kentencroth@yahoo.com>
2025-03-09 04:57:41 -0500

    Style Changes

---
+++ src/Components/HeartIcon.tsx
@@ -0,0 +1,22 @@
+interface HeartIconProps {
+  fill?: string;
+}
+
+export const HeartIcon: React.FC<HeartIconProps> = ({ fill }) => {
+  return (
+    <svg
+      width="30px"
+      height="30px"
+      viewBox="0 0 24 24"
+      fill={fill}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 21s-6.5-4.5-9.5-8C-.5 9 1 4.5 5 3c2.3-.9 4.9 0 7 2 2.1-2 4.7-2.9 7-2 4 1.5 5.5 6 2.5 10-3 3.5-9.5 8-9.5 8z"
+        stroke="#eb5e28"
+        strokeWidth="1.2"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
--- src/Components/Puppies/PuppiesCard.tsx
+++ src/Components/Puppies/PuppiesCard.tsx
@@ -1,23 +1,27 @@
 import { Dog } from "../../types";
+import { HeartIcon } from "../HeartIcon";
 
 interface IProps {
   puppy: Dog;
-  selectedFavorite: boolean;
-  onSelectFavorite: (id: string) => void;
 }
 
 export const PuppiesCard = (props: IProps) => {
   const { puppy } = props;
+  let fill = "#eb5e28";
+
   return (
     <div className="puppy-card">
+      <div className="puppy-card_heart">
+        <HeartIcon fill={fill} />
+      </div>
+      <p className="puppy-card_age">Age: {puppy.age}</p>
       <div className="puppy-card_image">
         <img src={puppy.img} alt="puppy" />
       </div>
-      <div className="puppy-card__info">
-        <h3>{puppy.name}</h3>
-        <p>{puppy.breed}</p>
-        <p>Age: {puppy.age}</p>
-        <p>Location: {puppy.zip_code}</p>
+      <div className="puppy-card_info">
+        <h3 className="puppy-card_info__name">{puppy.name}</h3>
+        <p className="puppy-card_info__breed">{puppy.breed}</p>
+        <p className="puppy-card_info__zip">Location: {puppy.zip_code}</p>
       </div>
     </div>
   );
